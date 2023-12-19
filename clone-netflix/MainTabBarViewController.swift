24d733be9edec2999dfb1a8234c19bee5db820d6
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-19 11:16:36 -0300

    working on

--- clone-netflix/MainTabBarViewController.swift
+++ clone-netflix/MainTabBarViewController.swift
@@ -19,9 +19,18 @@ class MainTabBarViewController: UITabBarController {
         let vc4 = UINavigationController(rootViewController: DownloadViewController())
         
         vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc1.title = "Home"
+        
         vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc2.title = "Coming Soon"
+        
         vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc3.title = "Top Search"
+        
         vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        vc4.title = "Downloads"
+        
+
         
         setViewControllers([vc1, vc2, vc3, vc4], animated: true)
         
