cdf76a3427c345b22933b48c4b5e4907fe5ef4cc
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-19 11:09:02 -0300

    working on

--- clone-netflix/MainTabBarViewController.swift
+++ clone-netflix/MainTabBarViewController.swift
@@ -13,6 +13,11 @@ class MainTabBarViewController: UITabBarController {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
         
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadViewController())
+        
     }
 
 
