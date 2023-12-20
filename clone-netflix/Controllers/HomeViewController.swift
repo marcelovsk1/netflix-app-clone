9ed043992fc467a4059be3ad642a98de5be81aa2
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-20 14:40:35 -0300

    working on

--- clone-netflix/Controllers/HomeViewController.swift
+++ clone-netflix/Controllers/HomeViewController.swift
@@ -29,7 +29,7 @@ class HomeViewController: UIViewController {
     }
     
     private func configureNavbar() {
-        var image = UIImage(named: "netflixvintage")
+        var image = UIImage(named: "N")
         image = image?.withRenderingMode(.alwaysOriginal)
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
         
