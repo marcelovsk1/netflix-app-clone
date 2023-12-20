a74c13c274e1cb89271fef8485267fa32ca5d4a2
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-20 09:30:09 -0300

    working on

---
+++ clone-netflix/Views/HeroHeaderUIView.swift
@@ -0,0 +1,34 @@
+//
+//  HeroHeaderUIView.swift
+//  clone-netflix
+//
+//  Created by Marcelo Amaral Alves on 2023-12-20.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
