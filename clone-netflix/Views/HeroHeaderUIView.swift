8eef5713b08b66638c54dfee65288b7a46c9cd27
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-20 10:06:15 -0300

    working on

--- clone-netflix/Views/HeroHeaderUIView.swift
+++ clone-netflix/Views/HeroHeaderUIView.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
     
+    private let playButton: UIButton = {
+        
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
-        imageView.image = UIImage(named: "heroImage")
+        imageView.image = UIImage(named: "heroImage1")
         return imageView
     }()
     
@@ -31,6 +41,15 @@ class HeroHeaderUIView: UIView {
         super.init(frame: frame)
         addSubview(heroImageView)
         addGradient()
+        addSubview(playButton)
+        applyConstrains()
+    }
+    
+    private func applyConstrains() {
+        
+        let playButtonConstrains = [
+            playButton.leadingAnchor
+        ]
     }
     
     override func layoutSubviews() {
