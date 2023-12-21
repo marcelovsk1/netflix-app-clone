da2c1e82e5ae3618f1ebb5fa7b7d9ec7c11ff078
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-21 09:53:57 -0300

    working on master

--- clone-netflix/Managers/APICaller.swift
+++ clone-netflix/Managers/APICaller.swift
@@ -26,9 +26,8 @@ class APICaller {
             }
             
             do {
-                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
                 print(results)
-                
             } catch {
                 print(error.localizedDescription)
             }
