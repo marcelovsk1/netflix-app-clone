dfcdd662776e14ce792b0140fc3882dad136bc55
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-21 09:39:25 -0300

    working on master

--- clone-netflix/Managers/APICaller.swift
+++ clone-netflix/Managers/APICaller.swift
@@ -9,10 +9,23 @@ import Foundation
 
 struct Constants {
     static let API_KEY = "532a65eff2df46ffcb01e4b766128daa"
+    static let baseURL = "https://api.themoviedb.org"
 }
 
 class APICaller {
     static let shared = APICaller()
     
-    
+    func getTrendingMovies(completion: @escaping (String) -> Void) {
+        
+        guard let url = URL(string: "\(Constants.baseURL)//3/trending/all/day?api_key=\(Constants.API_KEY)")
+        else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            
+        }
+    }
 }
