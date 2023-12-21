39c6043789ef90767683f7f414a55e68dcc5dc87
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-21 09:57:54 -0300

    working on master

--- clone-netflix/Managers/APICaller.swift
+++ clone-netflix/Managers/APICaller.swift
@@ -12,10 +12,14 @@ struct Constants {
     static let baseURL = "https://api.themoviedb.org"
 }
 
+enum APIError: Error {
+    case failedToGetData
+}
+
 class APICaller {
     static let shared = APICaller()
     
-    func getTrendingMovies(completion: @escaping (String) -> Void) {
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
         
         guard let url = URL(string: "\(Constants.baseURL)//3/trending/all/day?api_key=\(Constants.API_KEY)")
         else {return}
@@ -27,7 +31,7 @@ class APICaller {
             
             do {
                 let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
-                print(results)
+                completion(.success(results.results))
             } catch {
                 print(error.localizedDescription)
             }
