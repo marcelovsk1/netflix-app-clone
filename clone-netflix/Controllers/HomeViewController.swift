4ddb3b2879dd1f68632ce3cd37c18ea54a8ca30b
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-21 10:00:21 -0300

    working on master

--- clone-netflix/Controllers/HomeViewController.swift
+++ clone-netflix/Controllers/HomeViewController.swift
@@ -50,7 +50,14 @@ class HomeViewController: UIViewController {
     }
     
     private func getTrendingMovies() {
-        APICaller.shared.getTrendingMovies { _ in
+        APICaller.shared.getTrendingMovies { results in
+            switch results {
+                
+            case .success(let movies):
+                print(movies)
+            case .failure(let error):
+                print(error)
+        }
             
         }
     }
