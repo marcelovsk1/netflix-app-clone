d323fef97ece9aaea1d59d4d4a795fdc72ac698d
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-21 09:51:09 -0300

    working on master

--- clone-netflix/Controllers/HomeViewController.swift
+++ clone-netflix/Controllers/HomeViewController.swift
@@ -28,6 +28,8 @@ class HomeViewController: UIViewController {
         
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
         homeFeedTable.tableHeaderView = headerView
+        
+        getTrendingMovies()
     }
     
     private func configureNavbar() {
@@ -45,6 +47,12 @@ class HomeViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
+    }
+    
+    private func getTrendingMovies() {
+        APICaller.shared.getTrendingMovies { _ in
+            
+        }
     }
 }
 
---
+++ clone-netflix/Models/MoviesModel.swift
@@ -0,0 +1,50 @@
+//
+//  MoviesModel.swift
+//  clone-netflix
+//
+//  Created by Marcelo Amaral Alves on 2023-12-21.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse {
+    let results: [Movie]
+}
+
+struct Movie {
+    let id: Int
+    let media_type: String?
+    let original_name: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
+    let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+}
+
+/*
+ {
+adult = 0;
+"backdrop_path" = "/k1KrbaCMACQiq7EA0Yhw3bdzMv7.jpg";
+"genre_ids" =             (
+ 16,
+ 10751,
+ 10402,
+ 14,
+ 35
+);
+id = 901362;
+"media_type" = movie;
+"original_language" = en;
+"original_title" = "Trolls Band Together";
+overview = "When Branch's brother, Floyd, is kidnapped for his musical talents by a pair of nefarious pop-star villains, Branch and Poppy embark on a harrowing and emotional journey to reunite the other brothers and rescue Floyd from a fate even worse than pop-culture obscurity.";
+popularity = "782.173";
+"poster_path" = "/bkpPTZUdq31UGDovmszsg2CchiI.jpg";
+"release_date" = "2023-10-12";
+title = "Trolls Band Together";
+video = 0;
+"vote_average" = "7.15";
+"vote_count" = 391;
+},
+ */
