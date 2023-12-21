39d804f076b4140401411b3f4aefcc68b888549a
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-21 11:27:05 -0300

    working on master

--- clone-netflix/Managers/APICaller.swift
+++ clone-netflix/Managers/APICaller.swift
@@ -59,7 +59,7 @@ class APICaller {
     }
     
     func getUpcomingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
-        guard let url = URL(string: "\(Constants.baseURL)/3/upcoming?api_key=\(Constants.API_KEY)&language=en-US&page=1")
+        guard let url = URL(string: "\(Constants.baseURL)/3/movie/upcoming?api_key=\(Constants.API_KEY)&language=en-US&page=1")
         else {return}
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {
