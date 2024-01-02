e2e874d4c4730096f12b0f890e8e3a97ee325449
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-02 15:17:06 -0300

    working on 2024

--- clone-netflix/Controllers/UpcomingViewController.swift
+++ clone-netflix/Controllers/UpcomingViewController.swift
@@ -32,6 +32,12 @@ class UpcomingViewController: UIViewController {
 
 extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "test"
+        return cell
     }
 }
