735fc50909c3fa491bc469042924faac4e3c6aac
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-02 15:05:37 -0300

    working on 2024

--- clone-netflix/Controllers/UpcomingViewController.swift
+++ clone-netflix/Controllers/UpcomingViewController.swift
@@ -12,6 +12,9 @@ class UpcomingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
     }
     
 
